fe37ef7b27353183456ca8f05197950d1b12696d
Lucas <lucas@agtlucas.com>
2014-12-07 11:03:54 -0200

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 var koa = require('koa');
 var app = koa();
 var router = require('koa-router');
+
+app.use(router(app));
+
+var requestTime = function (headerName) {
+  return function *(next) {
+    var start = new Date();
+    yield next;
+    var end = new Date();
+    var ms = end - start;
+    this.set(headerName, ms - 'ms');
+  }
+}
